25aff7fac48ca1d36616ef41ccf3dc1017abfca9
Ali Gangji <ali@neonrain.com>
2017-04-12 18:43:07 -0600

    Initial Commit

---
+++ XMPPDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMPPDemo
+//
+//  Created by Ali Gangji on 4/12/17.
+//  Copyright © 2017 Neon Rain Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
